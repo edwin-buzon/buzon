7353973aae453574e3ea9dcbef9a54a5cc82fc41
edwin-buzon <44455742+edwin-buzon@users.noreply.github.com>
2018-10-25 00:09:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,260 +1,68 @@
-<?php 
-if (isset($_GET["created"])) {
-	echo "<div id='mensaje'>Su publico la noticia. </div>";
-}
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
 
- ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="utf-8">
-	<title>Primaria Julián Carrillo | Sitio Oficial</title>
-	<link rel="icon" href="imagenes/juli.ico">
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
-	<link rel="stylesheet" href="css/estilosslider.css">
-	<link rel="stylesheet" type="text/css" href="css/tabla.css">
-	<!-- DISEÑO DE ICONOS-->
-	<link rel="stylesheet" href="css/fontello.css">
-	<link rel="stylesheet" href="css/font-awesome.css">
-	<link rel="stylesheet" href="css/font-awesome.css">
-	<!--Fuente inportada desde googleapis-->
-	<link href="https://fonts.googleapis.com/css?family=Raleway:400,600" rel="stylesheet">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
-	<script src="js/jquery-3.1.0.min.js"></script>
-	<script src="js/main.js"></script>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link href="estilo.css" rel="stylesheet"  />
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="header.js"></script> 
+
+
+
 </head>
+
 <body>
-<!--INICIO DEL MENU PRINCIPAL-->
-	<header>
-		<div class="navegacion">
-		<nav>
-			<img id= logo src="imagenes/si.png">
-			<ul>
-				<!--SUBMENU EQUIPOS-->
-				<li><a href="#">Administrativos <span class="icon icon-up-dir"></span></a>
 
-					<div class="submenu">
-						<div class="submenu-items">
-							<p>Administrativos Activos</p>
-							<ul>
-								<li><a href="#">Directoa</a></li>
-								<li><a href="#">DocentesC</a></li>
-								<li><a href="#">Servicios Escolares</a></li>
-							</ul>
-						</div>
-
-					</div>
-
-				</li>
-
-				<!--LINK JUGADORES-->
-				<li>
-					<a href="#">Galeria</a></li>
-
-				<!--LINK TABLA GENERAL-->
-				
-				<!--SUBMENU CONTACTO-->
-				<li><a href="#">Oferta Educativa <span class="icon icon-up-dir"></span></a>
-
-					<div class="submenu">
-						<div class="submenu-items">
-							<p>Para más Información</p>
-							<ul>
-								<li><a href="#">Inscripciones</a></li>
-								<li><a href="#">Solicitud entrada</a></li>
-							</ul>
-						</div>
-					</div>
-
-				</li>
-
-				<!--SUBMENU SIGUENOS-->
-				<li><a href="#">Siguenos <span class="icon icon-up-dir"></span></a>
-
-					<div class="submenu">
-						<div class="submenu-items">
-							<p>Redes Sociales</p>
-							<ul>
-								<!--NOMENGLATURA PARA ABRIR PESTAÑAS EXTERNAS CON UN TAMAÑO DEFINIDO-->
-								<li><a href="https://twitter.com/?lang=es" target="popup" onClick="window.open(this.href, this.target, 'width=350,height=420'); return false;">Twitter</a></li>
-								<li><a href="https://www.facebook.com/" target="popup" onClick="window.open(this.href, this.target, 'width=350,height=420'); return false;">Facebook</a></li>
-								<li><a href="https://www.instagram.com/" target="popup" onClick="window.open(this.href, this.target, 'width=350,height=420'); return false;">Instagram</a></li>
-								<li><a href="https://www.youtube.com/" target="popup" onClick="window.open(this.href, this.target, 'width=350,height=420'); return false;">Youtube</a></li>
-							</ul>
-						</div>
-					</div>
-
-				</li>
-				<!--LINK INICIAR SESIÓN-->
-				<li id="sesion"><a href="login/login.php">Inicar Sesión</a></li>
-			</ul>
-		</nav>
-	</div>
-</header>
-<!--FINAL DEL MENU PRINCIPAL-->
-
-<!-- *************************************************************************************************** -->
-
-<!--INICIO DE SLIDER-->
-<div class="slideshow">
-		<ul class="slider">
-
-				<li>
-				<img src="imagenes/tres.jpg" alt="">
-				<section class="caption">
-					<a id="ligasanc" href=""><h1>Escuela Primaria Julián Carrillo</h1></a>
-					<p>Escuela primaria Jualián Carrillo, Tepeapulco Hidalgo</p>
-				</section>
-			</li>
-
-
-				<li> 
-				<img src="imagenes/cuatro.jpg" alt="">
-				<section class="caption">
-					<a id="ligaequipos" href=""><h1>Conferencias</h1></a>
-					<p>Conferencia Asistida por Carmen Lopez Sanchez</p>
-				</section>
-			</li>
-
-			<li>
-				<img src="imagenes/cinco.jpg" alt="">
-				<section class="caption">
-					<a id="ligalideres" href=""><h1>Graduación</h1></a>
-					<p>Graducación de los grupos de 6 año</p>
-				</section>
-			</li>
-
-			<li>
-				<img src="imagenes/seis.jpg" alt="">
-				<section class="caption">
-					<a id="ligatabla" href=""><h1>Padres de Familia</h1></a>
-					<p>Los padres simpre son el inicio de todo gran futuro</p>
-				</section>
-			</li>
-		</ul>
-
-		<div class="left">
-			<span class="fa fa-chevron-left"></span>
+	<header class="header">
+		<div class="wrapper1">
+			<div class="logo">Buzón  Sugerencias</div>
+			
+			<nav>
+				<a id="objetivo" href="#" >Objetivo</a>
+				<a id="agregar" href="#">Agregar sugerencias</a>
+				<a id="sugerencia" href="#">Sugerencias</a>
+				<a id="inicio" href="#">Inicio sesion</a>
+			</nav>
 		</div>
-
-		<div class="right">
-			<span class="fa fa-chevron-right"></span>
-		</div>
-
-	</div>
-<!-- *************************************************************************************************** -->
-	<div class="mapa">
-	<div id="main-container">
-		<h1>PROXIMOS EVENTOS </h1>
-		<table>
-			<thead>
-				<tr>
-					<th>Evento</th><th>Fechas</th><th>Hora</th><th>Mapa de como llegar</th>
-				</tr>
-			</thead>
-
-			<tr>
-				<td>Graduación Generación 2012-2018 </td><td>10/13/2017</td><td>15:00</td><td><a href="mapita/index.html">Ver Mapa</a></td>
-			</tr>
-			<tr>
-				<td>Desfilie por el día de la independencia.</td><td>10/13/2017</td><td>19:00</td><td><a href="mapita/index.html">Ver Mapa</a></td>
-			</tr>
-
-		</table>
-	</div>
-
-	</div>
-
-	<div>
-		<h1>MISIÓN</h1>
-		<h2>Garantizar que los educandos, adquieran los conocimientos básicos útiles para su vida diaria, mismos que establecen los planes y programas de Estudio en vigor, que logran desarrollar sus habilidades, destrezas intelectuales y por ende las competencias comunicativas, alumnos capaces de ubicar y analizar información acerca de los acontecimientos, hechos y procesos históricos y sociales, para comprender y explicar las características de la sociedad, así mismo nos llevara a formar a  nuestros alumnos, personas con valores bien definidos en beneficio propio y para  una mejor sociedad. 
-
-        Nuestra misión es desarrollar en los alumnos capacidades, habilidades, actitudes y valores para su formación integral: competencias fundamentales  para su incorporación a la sociedad y para el aprendizaje a lo largo de la vida. Todo por lograr los propósitos educativos.
-		</h2>
-	</div>
-
-		<div>
-		<h1>VISIÓN</h1>
-		<h2>Ser una institución educativa básica donde se imparta una educación integral, que cumpla y sirva de base para el interés de los alumnos, logrando una formación integral como seres humanos para un desarrollo pleno y armónico; siendo críticos, analíticos  y reflexivos, con valores  sólidos que le sirvan para enfrentar los retos de la vida futura tales como la: responsabilidad, eficacia, libertad, justicia, disciplina, patriotismo, ecología, entusiasmo, entre otros; con una planta de docentes capacitados y comprometidos con el devenir de la educación de la niñez mexicana, padres de familia enterados, motivados y colaborativos; los estudiantes conocerán su ambiente escolar y lo que se espera de ellos para el éxito de todos. A la vez, contar con la infraestructura necesaria para impartir una educación de calidad.
-		</h2>
-	</div>
-	<!--Iniciar el pie de pagina-->
-	<footer>
-
-		<div class="contenedor-all">
-    	<div class="contenedor-footer">
-    		
-    		<div class="columna1">
-    			
-
-    			<h1>Más información</h1>
-    			<p>Somos una escuela comprometida con nuestros estudiantes, sabiendo que ellos son el futuro de nuestro país.</p>
-
-    		</div>
-
-    	<div class="columna2">
-    			
-    			<h1>Redes Sociales</h1>
-    		<div class="redes">
-    			<img src="imagenes/facebook.png">
-    			<label><a href="">Siguenos en Facebook</a></label>
-    		
-    		</div>
-
-    		<div class="redes">
-    			<img src="imagenes/twitter.png">
-    			<label><a href="">Siguenos en Twitter</a></label>
-    		</div>
-
-    		<div class="redes">
-    			<img src="imagenes/instagram.png">
-    			<label><a href="">Siguenos en Instagram</a></label>
-    		</div>
-
-    		<div class="redes">
-    			<img src="imagenes/youtube.png">
-    			<label><a href="">Siguenos en YouTube</a></label>
-    		</div>
-    	</div>
-
-    	<div class="columna3">
-    		<h1>Indormación Contactos</h1>
-    		<div class="contac">
-    			<img src="imagenes/casa.png">
-    			<label>Tepeapulco, Pino Suárez placeCalle José María Pino Suárezo</label>
-    		</div>
-
-    		<div class="contac">
-    			<img src="imagenes/telefono.png">
-    			<label>+52 7751341232</label>
-    		</div>
-
-    		<div class="contac">
-    			<img src="imagenes/contacto.png">
-    			<label>juliancarrillo@gob.sep.mx</label>
-    		</div>
-    	</div>
-
-    </div>
-
-</div>
-
-   <div class="contenedor-footer2">
-   		<div class="footer">
-	    	<div class="derechos">
-	    		Copyright © 2017 Liga Sanctorum |
-	    		<a href="">Skynet</a>
-	    	</div>
-
-	    	<div class="terminos">
-	    		<a href="">Información Compañia </a> | <a href="">Privacidad y Politica</a> | <a href="">Terminos y Condiciones</a>
-	    	</div>
-	    	</div>
-    </div>
+	</header>
     
+    <section class="contenido wrapper1">
 
-</footer>
 
+	
+	<div class="div-objetivo">
+    	<h1 class="titulo" align="center"> Objetivo </h1> <br />
+        <hr  noshade="noshade" size="2" color="#f56f3a" />
+        
+  			<p style="font-size:1.5em; margin-bottom:50px;" ALIGN="center">Proporcionar un medio por el cual los estudiantes 				   			puedan presentar sus propuestas, <br /> recomendaciones e ideas que consideren oportunas para la institución.</p>
+	</div>
+    
+    <div class="div-agregar">
+    <h1 class="titulo" align="center"> Agregar Sugerencia </h1> <br />
+    <hr  noshade="noshade" size="2" color="#f56f3a" />
+    <div align="center"><iframe src="../modal/modal.html" width=99% height=735 frameborder=1 
+    style="border:solid #003b4a 2px; margin-bottom:5%;"></iframe></div>    
+	
 
+	</div>
+    <div class="div-sugerencia">
+     <h1 class="titulo" align="center"> Sugerencias </h1> <br />
+    <hr  noshade="noshade" size="2" color="#f56f3a" />
+    
+	<div align="center"><iframe src="../acordeon/acordeon.html" width=99% height=500 frameborder=1 
+    style="border:solid #003b4a 2px; margin-bottom:5%;"></iframe></div>
+    
+	</div>
+    
+  
+    </section>
 </body>
 </html>
